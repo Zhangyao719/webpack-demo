ea42f508c3dbf3ab2b89fafc63c2b539924de058
v_yaoxzhang <v_yaoxzhang@tencent.com>
2022-04-17 18:14:17 +0800

    feat: mini-css-extract-pulgin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+// css(style)-loader
 import add from './add-content.js'
 import './style/base.css'
+
+// html-loader
 import headerHtml from './html/header.html'
+
+// url-loader
 import gif from './assets/images/read.gif'
 import png from './assets/images/star.png'
 
+// mini-css-extract-plugins
+import './page1';
+
 add()
+
 document.write(headerHtml)
+
 console.log('🚀 → gif', gif)
 console.log('🚀 → png', png)
