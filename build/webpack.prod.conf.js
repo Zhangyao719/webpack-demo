789470429dc54d881e1335532025a0176d261f68
v_yaoxzhang <1258858403@qq.com>
2022-06-04 00:29:51 +0800

    Set env-variable

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,3 +1,21 @@
+const webpack = require("webpack")
+
 module.exports = {
     mode: 'production',
+
+    plugins: [
+        // 使用 DefinePlugin 添加环境变量：
+        new webpack.DefinePlugin({
+            /**
+             * * 启用 mode: 'production' 后, 自动添加，无需再重复设置。
+             * * DefinePlugin 在替换环境变量时对于字符串类型的值进行的是完全替换，
+             * * 不添加 JSON.stringify 在替换后会变成变量名，而非字符串。
+             */
+            'process.env.NODE_ENV': JSON.stringify('production'), // 等价于 mode: 'production'
+            IS_PRODUCTION: true,
+            CONSTANTS: JSON.stringify({
+                TYPES: ['foo', 'bar']
+            })
+        })
+    ]
 }
